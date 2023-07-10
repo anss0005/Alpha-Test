6eff620088ac926ce3617b29f23ff55d33ca39a7
shehz <anssraza41@gmail.com>
2023-07-10 21:41:17 +0500

    Commit 1(Main design)

---
+++ app/src/main/java/com/example/testalphabetic/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testalphabetic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
